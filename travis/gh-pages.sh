cf8e0d1c2f09eb65b9c206bd7412b6a4fa8e2987
signalwerk <sh@signalwerk.ch>
2018-12-25 04:21:43 +0100

    FIX: path

--- travis/gh-pages.sh
+++ travis/gh-pages.sh
@@ -81,7 +81,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 eval `ssh-agent -s`
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in "$ROOT_DIR/travis/.ssh/id_rsa.enc" -d | SSH_ASKPASS=$ROOT_DIR/ap-helper.sh DISPLAY= ssh-add -
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in "$ROOT_DIR/travis/.ssh/id_rsa.enc" -d | SSH_ASKPASS=$ROOT_DIR/travis/ap-helper.sh DISPLAY= ssh-add -
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
